875c0f63a1a3616d1fc930ff101603da64ed3341
Peter Evans <peter@baserow.io>
2022-09-02 11:16:26 +0100

    Letting make sort move the TokenAuthentication import.

--- backend/src/baserow/api/user_files/views.py
+++ backend/src/baserow/api/user_files/views.py
@@ -8,6 +8,7 @@ from rest_framework.views import APIView
 
 from baserow.api.decorators import map_exceptions, validate_body
 from baserow.api.schemas import get_error_schema
+from baserow.contrib.database.api.tokens.authentications import TokenAuthentication
 from baserow.core.user_files.exceptions import (
     FileSizeTooLargeError,
     FileURLCouldNotBeReached,
@@ -15,7 +16,6 @@ from baserow.core.user_files.exceptions import (
     InvalidFileURLError,
 )
 from baserow.core.user_files.handler import UserFileHandler
-from baserow.contrib.database.api.tokens.authentications import TokenAuthentication
 
 from .errors import (
     ERROR_FILE_SIZE_TOO_LARGE,
