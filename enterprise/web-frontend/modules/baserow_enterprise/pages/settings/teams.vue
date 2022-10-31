687bac73c6f024d53b13ef2b89bdfb84147b294f
peter_baserow <peter@baserow.io>
2022-10-31 11:35:29 +0000

    When the teams component mounts, add ENTERPRISE_ACTION_SCOPES.teams_in_group.

--- enterprise/web-frontend/modules/baserow_enterprise/pages/settings/teams.vue
+++ enterprise/web-frontend/modules/baserow_enterprise/pages/settings/teams.vue
@@ -4,6 +4,7 @@
 
 <script>
 import TeamsTable from '@baserow_enterprise/components/teams/TeamsTable'
+import { ENTERPRISE_ACTION_SCOPES } from '@baserow_enterprise/undoRedoConstants'
 
 export default {
   name: 'Teams',
@@ -13,6 +14,12 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  mounted() {
+    this.$store.dispatch(
+      'undoRedo/updateCurrentScopeSet',
+      ENTERPRISE_ACTION_SCOPES.teams_in_group(this.group.id)
+    )
   },
 }
 </script>
