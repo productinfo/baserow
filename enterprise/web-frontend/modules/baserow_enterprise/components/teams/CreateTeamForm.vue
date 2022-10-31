ec633a520dbdc469b7dcba5804762585d399a97b
peter_baserow <peter@baserow.io>
2022-10-31 11:34:33 +0000

    Adding support for creating a team with subjects.

--- enterprise/web-frontend/modules/baserow_enterprise/components/teams/CreateTeamForm.vue
+++ enterprise/web-frontend/modules/baserow_enterprise/components/teams/CreateTeamForm.vue
@@ -19,7 +19,27 @@
           </div>
         </FormElement>
       </div>
-      <slot></slot>
+    </div>
+    <h3>Users</h3>
+    <div class="row">
+      <div class="col col-8">
+        <FormElement class="control">
+          <div class="control__elements">
+            <input
+              ref="subjectSample"
+              type="text"
+              class="input"
+              v-model="subjectValue"
+              placeholder="Optionally choose who should be part of this team."
+              disabled
+            />
+          </div>
+        </FormElement>
+      </div>
+      <div class="col col-4 align-right">
+        <slot name="inviteButton"></slot>
+      </div>
+      <slot name="createButton"></slot>
     </div>
   </form>
 </template>
@@ -32,12 +52,25 @@ import form from '@baserow/modules/core/mixins/form'
 export default {
   name: 'CreateTeamForm',
   mixins: [form],
+  computed: {
+    subjectValue() {
+      let sample_names = this.subjects.map((subject) => subject.name)
+      if(sample_names.length > this.max_subjects) {
+        let beyond_sample = sample_names.length - this.max_subjects
+        sample_names = sample_names.splice(0, this.max_subjects)
+        return sample_names.join(', ') + ` and ${beyond_sample} more`
+      }
+      return sample_names.join(', ')
+    }
+  },
   data() {
     return {
       loading: false,
+      subjects: [],
+      max_subjects: 5,
       values: {
         name: '',
-      },
+      }
     }
   },
   validations: {
