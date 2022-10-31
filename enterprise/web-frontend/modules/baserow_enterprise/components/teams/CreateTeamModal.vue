44214a0631f89ac722c861df03be2aca44d15a94
peter_baserow <peter@baserow.io>
2022-10-31 11:34:57 +0000

    Adding support for creating teams with subjects.

--- enterprise/web-frontend/modules/baserow_enterprise/components/teams/CreateTeamModal.vue
+++ enterprise/web-frontend/modules/baserow_enterprise/components/teams/CreateTeamModal.vue
@@ -5,16 +5,32 @@
     </h2>
     <Error :error="error"></Error>
     <CreateTeamForm ref="createForm" @submitted="teamSubmitted">
-      <div class="col col-12 align-right">
-        <button
+      <template #inviteButton>
+        <a
           :class="{ 'button--loading': loading }"
           class="button"
           :disabled="loading"
-        >
-          {{ $t('teamsSettings.createTeamModal.submit') }}
-        </button>
-      </div>
+          @click="$refs.groupUserAssignmentModal.show()"
+          >{{ $t('MemberRolesDatabaseTab.selectMembers') }}
+      </a>
+      </template>
+      <template #createButton>
+        <div class="col col-12 align-right">
+          <button
+            :class="{ 'button--loading': loading }"
+            class="button"
+            :disabled="loading"
+          >
+            {{ $t('teamsSettings.createTeamModal.submit') }}
+          </button>
+        </div>
+      </template>
     </CreateTeamForm>
+    <GroupUserAssignmentModal
+      ref="groupUserAssignmentModal"
+      :users="getAllUsers"
+      @invite="storeSelectedUsers"
+     />
   </Modal>
 </template>
 
@@ -24,9 +40,12 @@ import error from '@baserow/modules/core/mixins/error'
 import { ResponseErrorMessage } from '@baserow/modules/core/plugins/clientHandler'
 import CreateTeamForm from '@baserow_enterprise/components/teams/CreateTeamForm'
 
+import { mapGetters } from 'vuex'
+import GroupUserAssignmentModal from '@baserow/modules/core/components/group/GroupUserAssignmentModal'
+
 export default {
   name: 'CreateTeamModal',
-  components: { CreateTeamForm },
+  components: { CreateTeamForm, GroupUserAssignmentModal },
   mixins: [modal, error],
   props: {
     group: {
@@ -37,6 +56,20 @@ export default {
   data() {
     return {
       loading: false,
+      subjectSelections: []
+    }
+  },
+  created() {
+    this.$store.dispatch('group/fetchAllGroupUser', {
+      groupId: this.group.id
+    })
+  },
+  computed: {
+    ...mapGetters({
+      members: 'group/getAllUsers',
+    }),
+    getAllUsers() {
+      return Object.values(this.members)
     }
   },
   methods: {
@@ -44,9 +77,31 @@ export default {
       this.hideError()
       modal.methods.show.bind(this)(...args)
     },
+    storeSelectedUsers(selections) {
+      selections.push({name: 'Alex'})
+      selections.push({name: 'Bram'})
+      selections.push({name: 'Jérémie'})
+      selections.push({name: 'Nigel'})
+      selections.push({name: 'Petr'})
+      selections.push({name: 'Clive'})
+      selections.push({name: 'Barry'})
+        this.subjectSelections = selections
+        this.$refs.createForm.subjects = selections
+    },
     async teamSubmitted(values) {
       this.createLoading = true
       this.hideError()
+      
+      if(this.subjectSelections.length) {
+        values['subjects'] = []
+        for(let s = 0; s < this.subjectSelections.length; s++) {
+          values['subjects'].push({
+              subject_id: this.subjectSelections[s].user_id,
+              subject_type: "auth_user"
+          })
+        }
+      }
+      console.log(values)
 
       try {
         const { team } = await this.$store.dispatch('team/create', {
