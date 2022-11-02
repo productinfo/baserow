2ac32c8f5faf959c104e61c40804f3a07a7a0d53
peter_baserow <peter@baserow.io>
2022-11-02 12:00:08 +0000

    Working on a modal which lets you pick group members from a list.

---
+++ web-frontend/modules/core/components/group/GroupUserAssignmentModalFooter.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="select-list-footer select-list-footer--single">
+    <a
+      class="button"
+      :class="{ disabled: !inviteEnabled }"
+      @click="inviteEnabled ? $emit('invite') : null"
+      >{{
+        $t('SelectListFooter.invite', {
+          count,
+          type: $t(`SelectListFooter.types.${type}`),
+        })
+      }}
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GroupUserAssignmentModalFooter',
+  props: {
+    count: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      validator: (type) => type === 'teams' || 'members',
+      default: 'members',
+    },
+  },
+  computed: {
+    inviteEnabled() {
+      return this.count !== 0
+    },
+  },
+}
+</script>
