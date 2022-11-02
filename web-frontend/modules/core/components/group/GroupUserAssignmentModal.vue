9d79f150449f5434a2437b1e3a0d7fe2d0a71fed
peter_baserow <peter@baserow.io>
2022-11-02 11:59:57 +0000

    Working on a modal which lets you pick group members from a list.

---
+++ web-frontend/modules/core/components/group/GroupUserAssignmentModal.vue
@@ -0,0 +1,33 @@
+<template>
+  <Modal>
+    <GroupUserSelectionList
+      ref="groupUserSelectionList"
+      class="margin-top-2"
+      :users="users"
+      @invite="storeSelectedUsers"
+    />
+  </Modal>
+</template>
+
+<script>
+import Modal from '@baserow/modules/core/mixins/modal'
+import GroupUserSelectionList from '@baserow/modules/core/components/group/GroupUserSelectionList'
+
+export default {
+  name: 'GroupUserAssignmentModal',
+  components: { GroupUserSelectionList },
+  mixins: [Modal],
+  props: {
+    users: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    storeSelectedUsers(usersSelected) {
+      this.$emit('invite', usersSelected)
+      this.hide()
+    },
+  },
+}
+</script>
