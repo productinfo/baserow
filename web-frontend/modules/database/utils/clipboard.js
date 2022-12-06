dbe770beea4a5958765f6158c15b3183bf1f2539
Davide Silvestri <davide@baserow.io>
2022-12-06 11:35:10 +0100

    Add function docs

--- web-frontend/modules/database/utils/clipboard.js
+++ web-frontend/modules/database/utils/clipboard.js
@@ -9,19 +9,33 @@ export const copyToClipboard = (text) => {
 const LOCAL_STORAGE_CLIPBOARD_KEY = 'baserow.clipboardData'
 
 /**
- * Values should be an object with mimetypes as key and clipboard content for this type
- * as value. This allow add the same data with multiple representation to the clipboard.
- * We can have a row saved as tsv string or as json string. Values must be strings.
- * @param {object} values object of mimetypes -> clipboard content.
+ * This methods sets the given text and json data in the clipboard and in the
+ * local storage respectively. This is needed because we need the original
+ * metadata to be able to restore the original rich format, but the clipboard only
+ * allows to store plain text, html or images related mime types.
+ * @param {string} text The text being copied in the clipboard.
+ * @param {object} json The rich json object with all the metadata needed to
+ * restore the original data in the correct format.
  */
-export const setRichClipboard = (text, jsonRawData) => {
+export const setRichClipboard = (text, json) => {
   copyToClipboard(text)
   localStorage.setItem(
     LOCAL_STORAGE_CLIPBOARD_KEY,
-    JSON.stringify({ text, jsonRawData })
+    JSON.stringify({ text, json })
   )
 }
 
+/**
+ * This method gets the text and json data from the clipboard and from the local
+ * storage. This is needed because we need the original metadata to be able to
+ * restore the original format, but the clipboard only allows to store plain
+ * text, html or images related mime types.
+ * @param {*} event
+ * @returns {object} An object with the textRawData and jsonRawData. The
+ * textRawData is the plain text that is stored in the clipboard. The
+ * jsonRawData is the rich json object with all the metadata needed to restore
+ * the original data in the correct rich format.
+ */
 export const getRichClipboard = (event) => {
   const textRawData = event.clipboardData.getData('text/plain')
   let jsonRawData
@@ -30,7 +44,7 @@ export const getRichClipboard = (event) => {
   try {
     clipboardData = JSON.parse(clipboardData)
     if (clipboardData.text === textRawData) {
-      jsonRawData = clipboardData.jsonRawData
+      jsonRawData = clipboardData.json
     } else {
       throw new Error(
         'Clipboard data is not the same as the local storage data'
