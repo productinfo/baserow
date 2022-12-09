f31cceeb9254db5148402d576621d4c28a3d2a77
alex1431999 <alex@baserow.io>
2022-12-09 16:53:39 +0100

    Add generic help icon component

---
+++ web-frontend/modules/core/components/HelpIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <i v-tooltip="tooltip" class="help_icon fas fa-question-circle"> </i>
+</template>
+
+<script>
+export default {
+  name: 'HelpIcon',
+  props: {
+    tooltip: {
+      type: String,
+      required: false,
+      default: null,
+    },
+  },
+}
+</script>
