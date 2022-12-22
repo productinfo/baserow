335fc07492f7b37f9515d61b5ec8c5fbe6a42f74
alex1431999 <alex@baserow.io>
2022-12-22 13:11:51 +0100

    Make sure the signal is ignored if RBAC isn't enabled

--- enterprise/backend/src/baserow_enterprise/role/receivers.py
+++ enterprise/backend/src/baserow_enterprise/role/receivers.py
@@ -3,16 +3,18 @@ from django.contrib.contenttypes.models import ContentType
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from baserow.core.models import GroupUser
+from baserow.core.models import GroupUser, Group
 from baserow.core.registries import subject_type_registry
 from baserow.core.signals import group_user_updated, permissions_updated
 from baserow.core.types import Subject
 from baserow.ws.tasks import broadcast_to_users
+from baserow_enterprise.features import RBAC
 from baserow_enterprise.signals import (
     role_assignment_created,
     role_assignment_deleted,
     role_assignment_updated,
 )
+from baserow_premium.license.handler import LicenseHandler
 
 User = get_user_model()
 
@@ -21,20 +23,25 @@ User = get_user_model()
 @receiver(role_assignment_created)
 @receiver(role_assignment_deleted)
 def send_permissions_updated_when_role_assignment_updated(
-    sender, subject: Subject, **kwargs
+    sender, subject: Subject, group: Group, **kwargs
 ):
-    permissions_updated.send(sender, subject=subject)
+    permissions_updated.send(sender, subject=subject, group=group)
 
 
 @receiver(group_user_updated)
 def send_permissions_updated_when_group_user_updated(
     sender, group_user: GroupUser, **kwargs
 ):
-    permissions_updated.send(sender, subject=group_user.user)
+    permissions_updated.send(sender, subject=group_user.user, group=group_user.group)
 
 
 @receiver(permissions_updated)
-def notify_users_about_updated_permissions(sender, subject: Subject, **kwargs):
+def notify_users_about_updated_permissions(
+    sender, subject: Subject, group: Group, **kwargs
+):
+    if not LicenseHandler.group_has_feature(RBAC, group):
+        return
+
     subject_type = subject_type_registry.get_by_model(subject)
     associated_users = subject_type.get_associated_users(subject)
     associated_user_ids = [user.id for user in associated_users]
